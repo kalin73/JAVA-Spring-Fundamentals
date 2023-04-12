e4bb960617e9671e01d0f3123d31f3c03e3db5b5
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-04-12 17:44:33 +0300

    fix

--- Mobilele/src/main/java/com/softuni/mobilele/web/UserController.java
+++ Mobilele/src/main/java/com/softuni/mobilele/web/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,7 +31,7 @@ public class UserController extends BaseController {
 	}
 
 	@PostMapping("/register")
-	public ModelAndView postRegister(UserRegisterFormDto userRegister) {
+	public ModelAndView postRegister(@ModelAttribute UserRegisterFormDto userRegister) {
 		
 		return super.redirect("auth-login");
 	}
